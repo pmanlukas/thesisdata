399ce5e8c2907fbf6f50bb9966880096a9c709fc
pmanlukas <lukas.pollmann@tum.de>
2018-04-21 15:19:56 +0200

    add prep for paths clustering

--- kmodes.R
+++ kmodes.R
@@ -1,5 +1,12 @@
-install.packages("klaR")
+set.seed(1680) # for reproducibility
+
+library(dplyr) # for data cleaning
+library(ISLR) # for college dataset
+library(cluster) # for gower similarity and pam
+library(Rtsne) # for t-SNE plot
+library(ggplot2) # for visualization
 library(klaR)
+
 setwd("C:/Users/lukas/PycharmProjects/monimporter")
 clusterdata <- read.csv(file="clusterdfr.csv", header=TRUE, sep=",")
 
@@ -9,11 +16,10 @@ str(clusterdata)
 head(clusterdata, n=10)
 
 
-data.to.cluster <- read.csv(file="clusterdfr.csv", header=TRUE, sep=",")
-cluster.results <- kmodes(data.to.cluster[,2:16], 6, iter.max = 10, weighted = FALSE)
-plot(data.to.cluster[,2:15],col= cluster.results$cluster)#
+cluster.results <- kmodes(clusterdata[,2:16], 6, iter.max = 10, weighted = FALSE)
+plot(clusterdata[,2:16],col= cluster.results$cluster)#
 
 cluster.results
 
-cluster.output <- cbind(data.to.cluster,cluster.results$cluster)
-write.csv(cluster.output, file = "kmodes clusters3.csv", row.names = TRUE)
+cluster.output <- cbind(clusterdata,cluster.results$cluster)
+write.csv(cluster.output, file = "kmodes clusters6.csv", row.names = TRUE)
