0fc663fdbc159f38f382706a3821b18e69b70fef
pmanlukas <lukas.pollmann@tum.de>
2018-05-18 15:12:43 +0200

    added new evaluation of k

--- grower3.R
+++ grower3.R
@@ -1,11 +1,7 @@
-
-
 #https://www.r-bloggers.com/clustering-mixed-data-types-in-r/
 
 set.seed(1680) # for reproducibility
 
-
-library(factoextra)
 library(dplyr) # for data cleaning
 library(ISLR) # for college dataset
 library(cluster) # for gower similarity and pam
@@ -15,26 +11,26 @@ library(ggplot2) # for visualization
 # Remove college name before clustering
 
 setwd("C:/Users/lukas/PycharmProjects/monimporter")
-clusterdata <- read.csv(file="comp_df.csv", header=TRUE, sep=",")
+clusterdata <- read.csv(file="cluster5.csv", header=TRUE, sep=",")
 
-clusterdata[, 2:22]
+clusterdata[, 2:18]
 
-gower_dist <- daisy(clusterdata[, 2:22],
+gower_dist <- daisy(clusterdata[, 2:18],
                     metric = "gower",
-                    type = list(logratio = 2))
+                    type = list(logratio = 3))
 
 summary(gower_dist)
 
 gower_mat <- as.matrix(gower_dist)
 
 #output most similar pair
-most_similar <- clusterdata[
+clusterdata[
   which(gower_mat == min(gower_mat[gower_mat != min(gower_mat)]),
         arr.ind = TRUE)[1, ], ]
 
 # Output most dissimilar pair
 
-least_similar <- clusterdata[
+clusterdata[
   which(gower_mat == max(gower_mat[gower_mat != max(gower_mat)]),
         arr.ind = TRUE)[1, ], ]
 
@@ -59,41 +55,10 @@ plot(1:20, sil_width,
      ylab = "Silhouette Width")
 lines(1:20, sil_width)
 
-###########
-#Other evaluation of optimal value for k
-#we use factoextra package and apply it on the gower_distance as matrix
-
-# Elbow method
-fviz_nbclust(gower_mat, pam, k.max = 20, method = "wss") +
-  geom_vline(xintercept = 4, linetype = 2)+
-  labs(subtitle = "Elbow method")
-
-# Silhouette method
-fviz_nbclust(gower_mat, pam, k.max = 20, method = "silhouette")+
-  labs(subtitle = "Silhouette method")
-
-# Gap statistic
-# nboot = 50 to keep the function speedy. 
-# recommended value: nboot= 500 for your analysis.
-# Use verbose = FALSE to hide computing progression.
-set.seed(123)
-fviz_nbclust(gower_mat, pam, k.max = 20, method = "gap_stat", nboot = 50)+
-  labs(subtitle = "Gap statistic method")
-
-
-# we could also do:
-library(fpc)
-asw <- numeric(20)
-for (k in 2:20)
-  asw[[k]] <- pam(gower_mat, k) $ silinfo $ avg.width
-k.best <- which.max(asw)
-cat("silhouette-optimal number of clusters:", k.best, "\n")
-
-#############
 #Cluster Interpretation
 #Via Descriptive Statistics
 
-pam_fit <- pam(gower_dist, diss = TRUE, k = 6)
+pam_fit <- pam(gower_dist, diss = TRUE, k = 4)
 
 pam_results <- clusterdata %>%
   dplyr::select(-X) %>%
@@ -123,4 +88,4 @@ pam_fit$clustering
 clusterdata[pam_fit$medoids, ]
 
 clusteroutput <- cbind(clusterdata,pam_fit$clustering)
-write.csv(clusteroutput, file = "PAMcomplete.csv", row.names = TRUE)
+write.csv(clusteroutput, file = "PAMmix.csv", row.names = TRUE)
